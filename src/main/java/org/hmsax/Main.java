c1893628a56d6d1c2d33a63bed526b4e1e8a5931
Martin Sax <h.martin.sax@gmail.com>
2026-08-20 12:25:27 +0200

    Edits to main

--- src/main/java/org/hmsax/Main.java
+++ src/main/java/org/hmsax/Main.java
@@ -4,14 +4,14 @@ package org.hmsax;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
+        System.out.print("Hello and welcome to \"Programmering med Java, grund\"!\n");
+        //Notera radbrytning med \n ovan. Nedan bjussar jag på en gratis for-loop
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            int countdownInt = 5 - i;
+            System.out.println(countdownInt + 1);
+            if (countdownInt == 0) {
+                System.out.println("TAKEOFF!");
+            }
         }
     }
 }
