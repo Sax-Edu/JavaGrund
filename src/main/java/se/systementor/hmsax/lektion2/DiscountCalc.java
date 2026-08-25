987b8c563cd51666a49a3ed40bdf5e2568cbb485
Martin Sax <martin.sax@systementor.se>
2026-08-25 17:52:37 +0200

    Added TempConverter and edited ScannerPractice

--- src/main/java/se/systementor/hmsax/lektion2/DiscountCalc.java
+++ src/main/java/se/systementor/hmsax/lektion2/DiscountCalc.java
@@ -1,6 +1,7 @@
 package se.systementor.hmsax.lektion2;
 
 public class DiscountCalc {
+
     public static void main(String[] args) {
         double price = 199.0;
         int discountPercent = 20; //Vad händer om vi sätter denna variabel större än 100?
