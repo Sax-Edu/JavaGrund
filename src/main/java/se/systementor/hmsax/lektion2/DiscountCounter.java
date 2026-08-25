cb55aa26155aeedd19583b971c0cebacfb3f804b
Martin Sax <h.martin.sax@gmail.com>
2026-08-25 11:05:24 +0200

    New DicountCalculator program with added comment

---
+++ src/main/java/se/systementor/hmsax/lektion2/DiscountCounter.java
@@ -0,0 +1,11 @@
+package se.systementor.hmsax.lektion2;
+
+public class DiscountCounter {
+    public static void main(String[] args) {
+        double price = 199.0;
+        int discountPercent = 20; //Vad händer om vi sätter denna variabel större än 100?
+        double discount = price * discountPercent / 100.0;
+        double finalPrice = price - discount;
+        System.out.println("Du betalar: " + finalPrice + " kr");
+    }
+}
