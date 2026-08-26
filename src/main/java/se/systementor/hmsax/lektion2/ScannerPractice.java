1887d176c9fa912db5e79f109cbe5ee52dd3127e
Martin Sax <martin.sax@systementor.se>
2026-08-26 16:28:23 +0200

    Updated ScannerPractice

--- src/main/java/se/systementor/hmsax/lektion2/ScannerPractice.java
+++ src/main/java/se/systementor/hmsax/lektion2/ScannerPractice.java
@@ -8,15 +8,12 @@ public class ScannerPractice {
         System.out.print("Vad heter du i förnamn? ");
         String firstName = scanner.nextLine();
         System.out.println("Hej " + firstName + "!");
-        scanner.close();
-    }
-}
-
-        /*
         System.out.print("Din ålder: ");
         int age = scanner.nextInt();
         scanner.nextLine(); //Viktig! Konsumerar kvarvarande line efter int.
         System.out.print("Ditt efternamn: ");
         String lastName = scanner.nextLine();
         System.out.println("Hej " + firstName + " " + lastName + ", du är " + age + " år.");
-        */
+        scanner.close();
+    }
+}
