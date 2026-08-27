b57483b7307e64685e85e737e575b95385e528e6
Martin Sax <martin.sax@systementor.se>
2026-08-27 18:05:11 +0200

    Added code for lesson 3

--- src/main/java/se/systementor/hmsax/lektion3/ovningsuppgifter/VG2NumberGuesser.java
+++ src/main/java/se/systementor/hmsax/lektion3/ovningsuppgifter/VG2NumberGuesser.java
@@ -1,13 +1,14 @@
 package se.systementor.hmsax.lektion3.ovningsuppgifter;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class VG2NumberGuesser {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int secret = (int) (Math.random() * 100) + 1;
-        int guesses = 0;
+        Random random = new Random();
+        int secret = random.nextInt(100) + 1;
+        int attempts = 0;
         int guess = 0;
 
         System.out.println("Jag tänker på ett tal mellan 1 och 100. Gissa!");
@@ -15,14 +16,14 @@ public class VG2NumberGuesser {
         while (guess != secret) {
             System.out.print("Din gissning: ");
             guess = scanner.nextInt();
-            guesses++;
+            attempts++;
 
             if (guess > secret) {
                 System.out.println("För högt!");
             } else if (guess < secret) {
                 System.out.println("För lågt!");
             } else {
-                System.out.println("Rätt! Du klarade det på " + guesses + " gissningar.");
+                System.out.println("Rätt! Du klarade det på " + attempts + " gissningar.");
             }
         }
 
