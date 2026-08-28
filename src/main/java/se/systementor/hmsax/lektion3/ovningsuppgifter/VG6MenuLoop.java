2605e53b7ab6323dc8639cb1e4109ef6c0f12a4c
Martin Sax <martin.sax@systementor.se>
2026-08-28 09:46:31 +0200

    Added code for lesson 3

--- src/main/java/se/systementor/hmsax/lektion3/ovningsuppgifter/VG6MenuLoop.java
+++ src/main/java/se/systementor/hmsax/lektion3/ovningsuppgifter/VG6MenuLoop.java
@@ -1,14 +1,16 @@
 package se.systementor.hmsax.lektion3.ovningsuppgifter;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class VG6MenuLoop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
         boolean running = true;
 
         while (running) {
-            System.out.println("=== Meny ==="); //Menyns 5 rader kan även placeras innan loopen
+            System.out.println("=== Meny ==="); //Menyns 5 rader kan även placeras innan loopen. Hur blir detta för UX?
             System.out.println("1. Hälsa");
             System.out.println("2. Räkna BMI");
             System.out.println("3. Slumpa tal");
@@ -42,7 +44,7 @@ public class VG6MenuLoop {
                     System.out.printf("Hej %s, ditt BMI är %.1f%n", namn, bmi);
                 }
                 case 3 -> {
-                    int randomNumber = (int) (Math.random() * 100) + 1;
+                    int randomNumber = random.nextInt(100) + 1;  //alternativ: int randomNumber = (int) (Math.random() * 100) + 1;
                     System.out.println("Slumpat tal: " + randomNumber);
                 }
                 case 4 -> {
