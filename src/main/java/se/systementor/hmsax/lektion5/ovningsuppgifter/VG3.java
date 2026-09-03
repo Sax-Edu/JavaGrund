631ceb66e56ef2c38431a263e8fca82bf42cd6ca
Martin Sax <martin.sax@systementor.se>
2026-09-03 14:37:31 +0200

    Added code for lesson 5 exercises

--- src/main/java/se/systementor/hmsax/lektion5/ovningsuppgifter/VG3.java
+++ src/main/java/se/systementor/hmsax/lektion5/ovningsuppgifter/VG3.java
@@ -1,6 +1,7 @@
 package se.systementor.hmsax.lektion5.ovningsuppgifter;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class VG3 {
     public static void main(String[] args) {
@@ -11,10 +12,12 @@ public class VG3 {
         phonebook.put("David", 52);
 
         // Sätt 1: Både namn (nyckel) och ålder (värde) samtidigt med forEach
-        System.out.println("--- Sätt 1: Både namn och ålder samtidigt ---");
-        phonebook.forEach((namn, alder) -> {
-            System.out.println(namn + " -> " + alder);
-        });
+        for (Map.Entry<String, Integer> entry : phonebook.entrySet()) {
+            String name = entry.getKey();
+            int age = entry.getValue();
+            System.out.println(name + " -> " + age);
+        }
+
 
         // Sätt 2: Hämtar först alla namn (keySet) och slår sedan upp åldern
         System.out.println("\n--- Sätt 2: Först alla namn ---");
