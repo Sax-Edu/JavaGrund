0bf2d9660627f371160aa013274dd63c0232161a
Martin Sax <martin.sax@systementor.se>
2026-09-07 14:06:38 +0200

    Added code for lesson 5 exercises

--- src/main/java/se/systementor/hmsax/lektion5/ovningsuppgifter/G3.java
+++ src/main/java/se/systementor/hmsax/lektion5/ovningsuppgifter/G3.java
@@ -12,6 +12,9 @@ public class G3 {
         list.add("Ost");
 
         System.out.println("Listan har " + list.size() + " varor:");
+        System.out.println(list.get(0) +", " + list.get(1) +", " + list.get(2) +", " + list.get(3) +", " + list.get(4));
+
+        //Alternativ, och snyggare, lösning för utskrift kommer här:
 
         StringBuilder joinedList = new StringBuilder();
 
@@ -23,13 +26,13 @@ public class G3 {
 
         System.out.println(joinedList);
 
-        // Alternativ för iteration över listan:
+        // Alternativ för iteration över listan och bygga ihop joinedList:
         // list.forEach(item -> joinedList.append(listItem).append(", "));
 
         String removed = list.remove(2); // Tar bort index 2 (tredje varan: Smör)
         System.out.println("Tog bort: " + removed);
 
         System.out.println("Listan har " + list.size() + " varor:");
-        System.out.println(String.join(", ", list)); //Alternativ till StringBuilder-lösningen
+        System.out.println(String.join(", ", list)); //Alternativ och ännu snyggare lösning till StringBuilder-lösningen
     }
 }
